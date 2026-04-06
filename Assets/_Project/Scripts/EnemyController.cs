75898f9172e7553144f326ca67243214c0fa593a
mohammedTouyak <mohammed.touyak@usmba.ac.ma>
2026-04-06 23:56:22 +0100

    feat(enemy): create enemy controller script

---
+++ Assets/_Project/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 3f;
+
+    [Header("Lifecycle Settings")]
+    [SerializeField] private float destroyY = -6f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
