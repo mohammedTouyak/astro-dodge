407e29e2fda3900c4966c9a44516dc705833922a
mohammedTouyak <mohammed.touyak@usmba.ac.ma>
2026-04-09 23:02:55 +0100

    feat(score): update score UI in real time

--- Assets/_Project/Scripts/ScoreManager.cs
+++ Assets/_Project/Scripts/ScoreManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
@@ -6,20 +7,35 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] private float currentScore = 0f; // why float ? Because later score may increase using time.
     [SerializeField] private float scoreRate = 10f;
 
+    [Header("UI References")]
+    [SerializeField] private TextMeshProUGUI scoreText;
+
     public float CurrentScore => currentScore;
 
     private void Start()
     {
         currentScore = 0f;
+        UpdateScoreUI();
     }
 
     private void Update()
     {
         IncreaseScoreOverTime();
+        UpdateScoreUI();
     }
 
     private void IncreaseScoreOverTime()
     {
         currentScore += scoreRate * Time.deltaTime;
+    }
+
+    private void UpdateScoreUI()
+    {
+        if (scoreText == null)
+        {
+            return;
+        }
+
+        scoreText.text = "Score: " + Mathf.FloorToInt(currentScore);
     }
 }
