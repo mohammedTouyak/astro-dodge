a5b7cb30df735def49f6ea30fd32b7cb4254ea9e
mohammedTouyak <mohammed.touyak@usmba.ac.ma>
2026-04-10 22:36:01 +0100

    feat(score): save best score locally

--- Assets/_Project/Scripts/ScoreManager.cs
+++ Assets/_Project/Scripts/ScoreManager.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    private const string BestScoreKey = "BestScore";
     [Header("Score Settings")]
     [SerializeField] private float currentScore = 0f; // why float ? Because later score may increase using time.
+    [SerializeField] private float bestScore = 0f;
     [SerializeField] private float scoreRate = 10f;
 
     [Header("UI References")]
@@ -13,11 +15,17 @@ public class ScoreManager : MonoBehaviour
     [Header("Gameplay References")]
     [SerializeField] private PlayerController playerController;
 
+    private bool hasSavedBestScore = false;
+
+
     public float CurrentScore => currentScore;
+    public float BestScore => bestScore;
 
     private void Start()
     {
         currentScore = 0f;
+        // PlayerPrefs.DeleteKey(BestScoreKey);
+        LoadBestScore();
         UpdateScoreUI();
     }
 
@@ -27,6 +35,11 @@ public class ScoreManager : MonoBehaviour
         {
             IncreaseScoreOverTime();
         }
+        else
+        {
+            SaveBestScoreIfNeeded();
+        }
+
         UpdateScoreUI();
     }
 
@@ -45,6 +58,28 @@ public class ScoreManager : MonoBehaviour
         currentScore += scoreRate * Time.deltaTime;
     }
 
+    private void LoadBestScore()
+    {
+        bestScore = PlayerPrefs.GetFloat(BestScoreKey, 0f);
+    }
+
+    private void SaveBestScoreIfNeeded()
+    {
+        if (hasSavedBestScore)
+        {
+            return;
+        }
+
+        if (currentScore > bestScore)
+        {
+            bestScore = currentScore;
+            PlayerPrefs.SetFloat(BestScoreKey, bestScore);
+            PlayerPrefs.Save();
+        }
+
+        hasSavedBestScore = true;
+    }
+
     private void UpdateScoreUI()
     {
         if (scoreText == null)
@@ -52,6 +87,6 @@ public class ScoreManager : MonoBehaviour
             return;
         }
 
-        scoreText.text = "Score: " + Mathf.FloorToInt(currentScore);
+        scoreText.text = "SCORE: " + Mathf.FloorToInt(currentScore);
     }
 }
